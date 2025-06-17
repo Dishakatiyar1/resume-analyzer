a2c5606c908f1f4633c1daa5d2f69eea868bf68d
Disha Katiyar <77583219+Dishakatiyar1@users.noreply.github.com>
2025-06-17 23:31:11 +0530

    code cleanup

--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-const multer = require("multer");
 const uploadRouter = require("./routes/uploadRouter");
 const connectDB = require("./config/database");
 require("dotenv").config();
@@ -10,9 +9,6 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-
-const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
 
 app.use("/api", uploadRouter);
 
